a972d9e0e791d741ffcc4f06a1e87a1bc0930dff
Viktoria <rybenchukv@gmail.com>
2023-04-19 23:38:01 +0200

    Added serializers for Movie model including MovieSerializer, MovieListSerializer, MovieDetailSerializer, and MovieImageSerializer.

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -5,9 +5,8 @@ from cinema.models import (
     Genre,
     Actor,
     CinemaHall,
-
+    Movie,
 )
-
 
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,3 +24,52 @@ class CinemaHallSerializer(serializers.ModelSerializer):
     class Meta:
         model = CinemaHall
         fields = ("id", "name", "rows", "seats_in_row", "capacity")
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = (
+            "id",
+            "title",
+            "description",
+            "duration",
+            "genres",
+            "actors",
+        )
+
+
+class MovieListSerializer(MovieSerializer):
+    genres = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="name"
+    )
+    actors = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="full_name"
+    )
+
+    class Meta:
+        model = Movie
+        fields = ("id", "title", "genres", "actors", "image")
+
+
+class MovieDetailSerializer(MovieSerializer):
+    genres = GenreSerializer(many=True, read_only=True)
+    actors = ActorSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Movie
+        fields = (
+            "id",
+            "title",
+            "duration",
+            "description",
+            "genres",
+            "actors",
+            "image",
+        )
+
+
+class MovieImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ("id", "image")
