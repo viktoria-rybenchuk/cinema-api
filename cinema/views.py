b88b7231dc8de93aa3903d153f6d814c0c6bf2f4
Viktoria <rybenchukv@gmail.com>
2023-04-19 23:58:39 +0200

    Added MovieViewSet class with List, Create, and Retrieve actions and refactored get_serializer_class method

--- cinema/views.py
+++ cinema/views.py
@@ -1,12 +1,12 @@
-from rest_framework import  mixins
+from rest_framework import mixins, viewsets
 from rest_framework.viewsets import GenericViewSet
 
-from cinema.models import Genre, Actor, CinemaHall
+from cinema.models import Genre, Actor, CinemaHall, Movie
 
 from cinema.serializers import (
     GenreSerializer,
     ActorSerializer,
-    CinemaHallSerializer,
+    CinemaHallSerializer, MovieSerializer, MovieListSerializer, MovieDetailSerializer, MovieImageSerializer,
 )
 
 
@@ -36,3 +36,23 @@ class CinemaHallViewSet(
 ):
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
+
+class MovieViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Movie.objects.prefetch_related("genres", "actors")
+    serializer_class = MovieSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieListSerializer
+
+        if self.action == "retrieve":
+            return MovieDetailSerializer
+
+        return MovieSerializer
+
+
