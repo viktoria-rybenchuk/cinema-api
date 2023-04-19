510e0a72fef8b91e70d058e159becba06bf2dc35
Viktoria <rybenchukv@gmail.com>
2023-04-19 23:53:12 +0200

    Added viewsets for Genre, Actor, and CinemaHall models with create and list mixins

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import  mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from cinema.models import Genre, Actor, CinemaHall
+
+from cinema.serializers import (
+    GenreSerializer,
+    ActorSerializer,
+    CinemaHallSerializer,
+)
+
+
+class GenreViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class ActorViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+
+class CinemaHallViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = CinemaHall.objects.all()
+    serializer_class = CinemaHallSerializer
