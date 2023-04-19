0da633683e33190edfef80ee4f7322d90eab45a4
Viktoria <rybenchukv@gmail.com>
2023-04-20 00:04:11 +0200

    Added upload image endpoint for movies

--- cinema/views.py
+++ cinema/views.py
@@ -1,10 +1,10 @@
-
-from rest_framework import viewsets, mixins
-
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.permissions import  IsAdminUser
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from cinema.models import Genre, Actor, CinemaHall, Movie
-
+from cinema.models import Genre, Actor, CinemaHall, Movie,
 
 from cinema.serializers import (
     GenreSerializer,
@@ -15,7 +15,6 @@ from cinema.serializers import (
     MovieListSerializer,
     MovieImageSerializer,
 )
-
 
 class GenreViewSet(
     mixins.CreateModelMixin,
@@ -91,7 +90,21 @@ class MovieViewSet(
 
         if self.action == "upload_image":
             return MovieImageSerializer
-
         return MovieSerializer
 
+    @action(
+        methods=["POST"],
+        detail=True,
+        url_path="upload-image",
+        permission_classes=[IsAdminUser],
+    )
+    def upload_image(self, request, pk=None):
+        """Endpoint for uploading image to specific movie"""
+        movie = self.get_object()
+        serializer = self.get_serializer(movie, data=request.data)
 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
