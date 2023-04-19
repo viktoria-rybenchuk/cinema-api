2ac88255edda24e5d7a46fa8721eaf455fac7809
Viktoria <rybenchukv@gmail.com>
2023-04-19 23:10:08 +0200

    Included string representation and meta information for each model

--- cinema/models.py
+++ cinema/models.py
@@ -1,8 +1,6 @@
-
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
-
 
 
 class CinemaHall(models.Model):
@@ -10,14 +8,32 @@ class CinemaHall(models.Model):
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
 
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return self.name
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 
 class Movie(models.Model):
@@ -27,12 +43,23 @@ class Movie(models.Model):
     genres = models.ManyToManyField(Genre, blank=True)
     actors = models.ManyToManyField(Actor, blank=True)
 
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
 
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ["-show_time"]
+
+    def __str__(self):
+        return self.movie.title + " " + str(self.show_time)
 
 
 class Order(models.Model):
@@ -41,6 +68,11 @@ class Order(models.Model):
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
 
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
 
 
 class Ticket(models.Model):
@@ -89,3 +121,12 @@ class Ticket(models.Model):
         return super(Ticket, self).save(
             force_insert, force_update, using, update_fields
         )
+
+    def __str__(self):
+        return (
+            f"{str(self.movie_session)} (row: {self.row}, seat: {self.seat})"
+        )
+
+    class Meta:
+        unique_together = ("movie_session", "row", "seat")
+        ordering = ["row", "seat"]
