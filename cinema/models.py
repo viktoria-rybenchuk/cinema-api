7389601e4eef657f44b039f6c584a4201b60c1a7
Viktoria <rybenchukv@gmail.com>
2023-04-19 23:05:50 +0200

    Added CinemaHall, Genre, Actor, Movie, MovieSession, Order, and Ticket models

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,53 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    duration = models.IntegerField()
+    genres = models.ManyToManyField(Genre, blank=True)
+    actors = models.ManyToManyField(Actor, blank=True)
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    cinema_hall = models.ForeignKey(CinemaHall, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+
+class Ticket(models.Model):
+    movie_session = models.ForeignKey(
+        MovieSession, on_delete=models.CASCADE, related_name="tickets"
+    )
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="tickets"
+    )
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
