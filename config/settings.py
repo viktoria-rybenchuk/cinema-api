13f056278ba3f3e2a49b43b440ebc4dc3e5e7649
Viktoria <rybenchukv@gmail.com>
2023-04-20 00:52:10 +0200

    Added rate limiting to DRF API with AnonRateThrottle and UserRateThrottle

--- config/settings.py
+++ config/settings.py
@@ -132,4 +132,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "10/day", "user": "30/day"},
 }
