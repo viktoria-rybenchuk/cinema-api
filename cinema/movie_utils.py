06ada8f6fd2377446fa5324261740260f672e05e
viktoria-rybenchuk <rybenchukv@gmail.com>
2023-04-28 18:51:45 +0200

    Refactored environment variables

---
+++ cinema/movie_utils.py
@@ -0,0 +1,12 @@
+import os
+import uuid
+
+from django.urls import path
+from django.utils.text import slugify
+
+
+def movie_image_file_path(instance: object, filename: str) -> path:
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/movies/", filename)
